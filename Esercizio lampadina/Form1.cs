cbf5a6a35cf65ca2f45e98a00960a9becb1c8e3b
0HU-UH0 <leohuyunjie@gmail.com>
2024-09-30 09:20:17 +0200

    Add files via upload

---
+++ Esercizio lampadina/Form1.cs
@@ -0,0 +1,20 @@
+namespace Esercizio_lampadina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Yellow;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.BackColor= Color.Gray;
+        }
+    }
+}
